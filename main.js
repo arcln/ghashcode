f98706d61e7bb5424a6967bb1e288b8471f8e48f
Arthur Chaloin <arthur.chaloin@epitech.eu>
2018-03-01 21:16:43 +0100

    writeer

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const Parser = require('./parser');
 const Vehicle = require('./map').Vehicle;
 const Solution = require('./solution');
+const fs = require('fs');
 
 function evalRun(map, solution) {
 	let finalPoints = 0;
@@ -16,14 +17,44 @@ function evalRun(map, solution) {
 
 		finalPoints += points;
 	});
+
+	return finalPoints;
+}
+
+function dumpRun(map, solution) {
+	let out = "";
+
+	solution.vehicles.forEach(vehicle => {
+		vehicle.rides = [];
+		vehicle.steps.forEach(step => {
+			if (step) {
+				vehicle.rides.push(step.id);
+			}
+		});
+
+		const line = vehicles.rides.length + " " + vehicles.rides.join();
+		out += line + '\n';
+	});
+
+	fs.delete('response.txt');
+	fs.appendFile('response.txt', out);
 }
 
 let map = Parser.parse('maps/map.txt');
 let solution = new Solution(map);
 let vehicles = [];
+let runs = [];
 for (let i = 0; i < map.vehicles; i++) {
 	vehicles.push(new Vehicle);
 }
 
-solution.compute();
-evalRun(map, solution);
+let bestRun = 0;
+for (let i = 0;; i++) {
+	solution.compute();
+	const val = evalRun(map, solution);
+	if (val > bestRun) {
+		console.log('Found better solution at iteration ' + i + ': ' + val + ' (old: ' + bestRun + ')');
+		bestRun = val;
+		dumpRun(map, solution);
+	}
+}
