0606dcf4fdfaf88c510fb81ab2713494d2a4deea
Arthur Chaloin <arthur.chaloin@epitech.eu>
2018-03-01 20:57:20 +0100

    merge

--- main.js
+++ main.js
@@ -2,11 +2,28 @@
 
 const Parser = require('./parser');
 const Vehicle = require('./map').Vehicle;
+const Solution = require('./solution');
 
-let map = Parser.parse('./maps/map.txt');
-console.log(map);
+function evalRun(map, solution) {
+	let finalPoints = 0;
 
+	solution.rides.forEach(ride => {
+		let points = ride.endTime - ride.startTime;
+
+		if (ride.startTime === 0) {
+			points += map.bonus;
+		}
+
+		finalPoints += points;
+	});
+}
+
+let map = Parser.parse('maps/map.txt');
+let solution = new Solution(map);
 let vehicles = [];
 for (let i = 0; i < map.vehicles; i++) {
 	vehicles.push(new Vehicle);
 }
+
+solution.compute();
+evalRun(map, solution);
